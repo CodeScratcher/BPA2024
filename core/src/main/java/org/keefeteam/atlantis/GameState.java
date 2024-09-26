1c4f9e41407ce053d4f02220415e24c2cbb8017d
CodeScratcher <arialexanderfisher@outlook.com>
2024-09-26 13:32:25 -0400

    Set up main loop

--- core/src/main/java/org/keefeteam/atlantis/GameState.java
+++ core/src/main/java/org/keefeteam/atlantis/GameState.java
@@ -2,18 +2,22 @@ package org.keefeteam.atlantis;
 
 import com.badlogic.gdx.Gdx;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class GameState {
-    private List<Entity> entities;
-    private float delta;
+    @NonNull private List<Entity> entities;
+    private float delta = 0.0f;
 
     public void update(List<InputEvent> events) {
         delta = Gdx.graphics.getDeltaTime();
+
         for (Entity entity : entities) {
             entity.update(this, events);
         }
--- core/src/main/java/org/keefeteam/atlantis/Main.java
+++ core/src/main/java/org/keefeteam/atlantis/Main.java
@@ -1,19 +1,35 @@
 package org.keefeteam.atlantis;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private GameState gameState;
+    private ArrayList<Entity> entities;
+    private Player player; // TODO REMOVE AND REPLACE LOGIC FOR RENDERING, JUST FOR TESTING
+    private Controller controller;
 
     @Override
     public void create() {
+        controller = new Controller();
+
+        player = new Player();
+        entities = new ArrayList<>();
+        entities.add(player);
+
+        gameState = new GameState(entities);
+
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
     }
@@ -25,10 +41,12 @@ public class Main extends ApplicationAdapter {
         // https://gameprogrammingpatterns.com/game-loop.html
         // https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
         // Render happens once per frame, so thank god we don't have to handle the full loop, just the internals
+        List<InputEvent> eventList = controller.getEvents();
+        gameState.update(eventList);
 
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, player.x, player.y);
         batch.end();
     }
 
