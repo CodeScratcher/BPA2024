9d8f195e139814ef9fe12e8e4dc6f9c29ca1c502
CodeScratcher <arialexanderfisher@outlook.com>
2024-10-24 09:45:33 -0400

    Added camera

---
+++ core/src/main/java/org/keefeteam/atlantis/Camera.java
@@ -0,0 +1,10 @@
+package org.keefeteam.atlantis;
+
+import com.badlogic.gdx.graphics.Texture;
+import org.keefeteam.atlantis.coordinates.WorldCoordinate;
+
+public class Camera {
+    public void renderImage(Texture texture, WorldCoordinate position) {
+        // TODO: Roman
+    }
+}
