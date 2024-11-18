2daa79ce00ada77835600363184b4263cf36ba92
CodeScratcher <arialexanderfisher@outlook.com>
2024-11-18 12:46:48 -0500

    Almost perfect collisions

--- core/src/main/java/org/keefeteam/atlantis/Player.java
+++ core/src/main/java/org/keefeteam/atlantis/Player.java
@@ -23,10 +23,14 @@ public class Player implements Entity, Renderable {
     private Texture texture;
 
     public static final int PLAYER_SPEED = 300;
+    public static final float REPAIR_SPEED = 0.1f;
 
     public List<Triangle> getTris() {
+        return getTris(position.getCoord());
+    }
+
+    public List<Triangle> getTris(Vector2 p1) {
         List<Triangle> tris = new ArrayList<Triangle>();
-        Vector2 p1 = position.getCoord();
         Vector2 p2 = new Vector2(p1.x + 64, p1.y);
         Vector2 p3 = new Vector2(p1.x, p1.y + 64);
         Vector2 p4 = new Vector2(p2.x, p3.y);
@@ -60,10 +64,28 @@ public class Player implements Entity, Renderable {
         boolean colliding = false;
         for (Entity entity : gameState.getEntities()) {
             if (entity instanceof Collider collider) {
-                if (collider.getColliderTypes().contains(Collider.ColliderTypes.WALL) && collider.collidesWith(getTris())) {
-                    System.out.println("Colliding");
+                if (collider.getColliderTypes().contains(Collider.ColliderTypes.WALL) && collider.collidesWith(getTris()) && !posChange.equals(new Vector2(0, 0))) {
                     colliding = true;
-                    WorldCoordinate.addWorldCoordinates(position, new WorldCoordinate(posChange.nor().scl(-PLAYER_SPEED * gameState.getDelta())));
+                    float repairX = 0;
+                    float repairY = 0;
+
+                    while (collider.collidesWith(getTris(new Vector2(position.getCoord().x + repairX, position.getCoord().y)))) {
+                        repairX -= REPAIR_SPEED * (posChange.x == 0 ? 10000000 : (posChange.x / Math.abs(posChange.x)));
+                    }
+
+                    while (collider.collidesWith(getTris(new Vector2(position.getCoord().x , position.getCoord().y + repairY)))) {
+                        repairY -= REPAIR_SPEED * (posChange.y == 0 ? 10000000 : (posChange.y / Math.abs(posChange.y)));
+                    }
+
+                    if (Math.abs(repairX) > Math.abs(repairY)) {
+                        position.getCoord().y += repairY;
+                    }
+                    else if (Math.abs(repairY) > Math.abs(repairX)) {
+                        position.getCoord().x += repairX;
+                    }
+                    else {
+                        position = WorldCoordinate.addWorldCoordinates(position, new WorldCoordinate(new Vector2(repairX, repairY)));
+                    }
                 }
             }
         }
