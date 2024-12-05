86b5686b8d42d80b27b95b0e129868e6ca60b884
RomanNoodles00 <rcriscenzo2026@jpkeefehs.org>
2024-12-05 13:35:46 -0500

    Added comments in Camera.java

--- core/src/main/java/org/keefeteam/atlantis/Camera.java
+++ core/src/main/java/org/keefeteam/atlantis/Camera.java
@@ -1,10 +1,36 @@
 package org.keefeteam.atlantis;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import lombok.Getter;
 import org.keefeteam.atlantis.coordinates.WorldCoordinate;
 
-public class Camera {
-    public void renderImage(Texture texture, WorldCoordinate position) {
+import java.util.List;
+
+
+public class Camera implements Entity {
+    int follow;
+    WorldCoordinate pos;
+    public void renderImage(SpriteBatch batch, Texture texture, WorldCoordinate position) {
         // TODO: Roman
+        batch.draw(texture, position.getCoord().x, position.getCoord().y);
+        //X is a coord on the screen not the world. WorldCoodinate is for the world, translate the world coordinate to
+        //the x and y
+
+        //look up libgdx screen coordinates
+        //screen cordinates start at top left
+        //world coodinates start at bottom left
+
+        //camera has its own coordinates (WorldCoordinate pos)
+
+    }
+
+    @Override
+    public void update(GameState gameState, List<InputEvent> events) {
+
+    }
+    public Vector2 convertCoordinates(WorldCoordinate c){
+        return null;
     }
 }
