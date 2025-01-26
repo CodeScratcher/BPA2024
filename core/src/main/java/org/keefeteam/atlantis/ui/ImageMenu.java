6be82fecd62ee9ae8f56bfdf9f6910c48dea4d27
Arielle Fisher <bb8andr2d2forlife@gmail.com>
2025-01-26 17:54:33 -0500

    imagemenu

--- core/src/main/java/org/keefeteam/atlantis/ui/ImageMenu.java
+++ core/src/main/java/org/keefeteam/atlantis/ui/ImageMenu.java
@@ -3,8 +3,10 @@ package org.keefeteam.atlantis.ui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -13,90 +15,44 @@ import org.keefeteam.atlantis.util.input.InputEvent;
 
 import java.util.*;
 
-public class DialogueMenu implements Menu {
+public class ImageMenu implements Menu {
     // The text that is being displayed
-    private String text;
-    // the maximum number of characters to be displayed
-    private int maxLength;
-    // where the text starts
-    private int textIndexOne;
-    // where the text ends
-    private int textIndexTwo;
-    // checks if there is no more text to display
-    private boolean textFinished;
-    boolean inputControl;
-
+    private Texture image;
     private Stage stage;
     private Table table;
     private Skin skin;
     private GameState gameState;
 
     //Where the text is printed
-    private Label textLabel;
 
-    public DialogueMenu(String t){
-        this.text = t;
+    public ImageMenu(Texture i){
+        this.image = i;
     }
 
     @Override
     public void initialize(GameState g) {
 
-        maxLength = 50;
         this.gameState = g;
         this.gameState.setPaused(true);
         stage = new Stage();
 
-        BitmapFont font = new BitmapFont();
-        skin = new Skin(Gdx.files.internal("ui/pixthulhu-ui.json"));
         Gdx.input.setInputProcessor(stage);
         table = new Table();
         table.setFillParent(true);
-        String firstOutput = "";
-        if(text.length() > maxLength){
-            firstOutput =  text.substring(0, maxLength);
-            this.textFinished = false;
-        }
-        else{
-            firstOutput = text;
-            this.textFinished = true;
-        }
-
-        textLabel = new Label(firstOutput, skin);
-
-        table.add(textLabel);
+        table.add(new Image(image));
         table.setFillParent(true);
         stage.addActor(table);
     }
 
     @Override
     public void update(Set<InputEvent> inputEvents) {
-        if (inputEvents.contains(InputEvent.Interact)) {
-            System.out.println(this.text);
-        }
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+        
         if(inputEvents.contains(InputEvent.UIConfirm)){
-            if(!textFinished){
-                textIndexOne += maxLength;
-                textIndexTwo = textIndexOne + maxLength;
-                if(textIndexTwo > text.length()){
-                    textIndexTwo = text.length();
-                    textFinished = true;
-                }
-                String newValue = text.substring(textIndexOne, textIndexTwo);
-                table.remove(); // Safely remove the old table
-                textLabel = new Label(newValue, skin);
-                table = new Table();
-                table.setFillParent(true);
-                table.add(textLabel);
-                stage.addActor(table);
-            }
-            else{
-                //Destroy label
-                table.remove();
-                gameState.setMenu(null);
-                this.gameState.setPaused(false);
-            }
+            table.remove();
+            gameState.setMenu(null);
+            this.gameState.setPaused(false);
         }
 
     }
