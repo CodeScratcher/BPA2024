a7a73b9b0dfc337d50c55369dbb0df8eadcaac29
CodeScratcher <arialexanderfisher@outlook.com>
2025-01-31 08:21:04 -0500

    main menu

--- core/src/main/java/org/keefeteam/atlantis/ui/MainMenu.java
+++ core/src/main/java/org/keefeteam/atlantis/ui/MainMenu.java
@@ -9,13 +9,12 @@ public class MainMenu extends ChoiceMenu {
     public MainMenu() {
         super(List.of("Start", "Help", "Exit"), (s, gameState) -> {
             switch (s) {
-                case "Start":
-                    break;
                 case "Help":
-                    DialogueMenu dialogueMenu = new DialogueMenu("", () -> {
+                    DialogueMenu dialogueMenu = new DialogueMenu("INSERT HELP TEXT HERE", () -> {
                         gameState.setMenu(new MainMenu());
                         return true;
                     });
+                    gameState.setMenu(dialogueMenu);
                     break;
                 case "Exit":
                     System.exit(0);
